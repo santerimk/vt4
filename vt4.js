1f8e036bdd8a62f5767ce66e556047b7af461c52
santerimk <santerimikkonen97@gmail.com>
2022-07-27 05:41:13 +0300

    eka commit

---
+++ vt4.js
@@ -0,0 +1,17 @@
+"use strict";
+//@ts-check 
+
+let palkkimaara = 10;
+
+let img = new Image();
+img.src = 'http://appro.mit.jyu.fi/tiea2120/vt/vt4/owl.png';
+img.alt = 'Pollo';
+img.title = 'pollo';
+
+window.onload = () => {
+    //
+};
+
+window.onresize = () => {
+    //
+};
